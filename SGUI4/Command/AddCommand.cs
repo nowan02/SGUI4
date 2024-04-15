474a16ac06d497767d63e3aeebe94e6bd6556523
nowan02 <starkgeri@gmail.com>
2024-04-15 12:53:52 +0200

    Added base classes

---
+++ SGUI4/Command/AddCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.ViewModel;
+using System.ComponentModel;
+
+namespace ToDoList.Command
+{
+    internal class AddCommand : BaseCommand
+    {
+        protected BasicVM viewModel { get; }
+        public AddCommand(BasicVM viewModel)
+        {
+            this.viewModel = viewModel;
+            this.viewModel.PropertyChanged += ViewModelPropertyChanged;
+        }
+        public override void Execute(object? parameter)
+        {
+            BasicVM.ObservableColl.Add(new BasicVM());
+            viewModel.OnPropertyChanged(nameof(BasicVM.ObservableColl));
+        }
+
+        protected void ViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(BasicVM.ObservableColl)) OnCanExecuteChanged();
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return base.CanExecute(parameter);
+        }
+    }
+}
---
+++ SGUI4/ViewModel/BasicVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.ViewModel
+{
+    public class BasicVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // List can be global if left here
+        public static ObservableCollection<BasicVM> ObservableColl { get; set; } = new ObservableCollection<BasicVM>();
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+
+            if (handler != null) handler(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
