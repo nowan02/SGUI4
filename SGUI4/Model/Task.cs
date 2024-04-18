de768fc829a9bec8519053cd7ec795aba48af414
szatmary.sara@stud.uni-obuda.hu <szatmary.sara@stud.uni-obuda.hu>
2024-04-18 15:33:31 +0200

    Added properties to Task

--- SGUI4/Model/Task.cs
+++ SGUI4/Model/Task.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SGUI4.Model
 {
-    internal class Task
+    public class Task
     {
+        public string Name { get; set; }
+        public int Deadline { get; set; } //num of days left
+        public string Type { get; set; } //Important / Not Important
+        public string Description { get; set; } //extra thing
+
+        public Task(string name, int deadline, string type, string descrition)
+        {
+            Name = name;
+            Deadline = deadline;
+            Type = type;
+            Description = descrition;
+        }
+
     }
 }
